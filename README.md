3cb7dd71b5238cb968d0a1f2a3204c3181813c1b
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-06-27 17:07:05 +0200

    Add computerPlay function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock-paper-scissors
+
+An implementation of a simple 'rock paper scissors' game.
+Currently without GUI.
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
